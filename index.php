35bd7adf5f60b11ab18bd34e9e371872af2aa314
vinnvi <vin.doisneau@free.fr>
2015-12-06 11:03:50 +0100

    changement nom identifiant

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                 <form action="index.php" method="post">
                   <div class="row">
                     <div class="small-12 columns">
-                      <label>Identifiant (Numéro étudiant)
+                      <label>Identifiant
                         <input type="text" placeholder="" />
                       </label>
                     </div>

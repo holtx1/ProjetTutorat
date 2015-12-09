b18777bd374bcc0a859ca1e5db673e4a8c2e3d68
Tilkeii <imtilkeii@gmail.com>
2015-12-09 22:22:15 +0100

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -7,31 +7,31 @@ DROP TABLE matiere;
 DROP TABLE privileges;*/
 
 CREATE TABLE privileges (
-  id_priv INT(4) PRIMARY KEY,
-  nom_priv VARCHAR(30)
+  id_priv INT(4) PRIMARY KEY AUTO_INCREMENT,
+  nom_priv VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE matiere (
-  id_mat INT(4) PRIMARY KEY,
-  nom_mat VARCHAR(30)
+  id_mat INT(4) PRIMARY KEY AUTO_INCREMENT,
+  nom_mat VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE groupe (
-  id_grp INT(4) PRIMARY KEY,
-  filiere VARCHAR(30),
-  annee DATE
+  id_grp INT(4) PRIMARY KEY AUTO_INCREMENT,
+  filiere VARCHAR(30) NOT NULL,
+  annee INT NOT NULL NOT NULL
 );
 
 CREATE TABLE etudiant (
   numero_etudiant INT(8) PRIMARY KEY,
-  mdp VARCHAR(25),
-  nom VARCHAR(30),
-  prenom VARCHAR(30),
+  mdp VARCHAR(25) NOT NULL,
+  nom VARCHAR(30) NOT NULL,
+  prenom VARCHAR(30) NOT NULL,
   age INT(3),
-  email VARCHAR(30),
-  id_priv INT(4) REFERENCES privileges,
-  id_grp INT(4) REFERENCES groupe
-);
+  email VARCHAR(30) NOT NULL,
+  id_priv INT(4) REFERENCES privileges, // NOT NULL ne fonctionne pas ici alors que sa fonctionne lorsque l'on modifie sur phpmyadmin
+  id_grp INT(4) REFERENCES groupe       // THE FUCK ! Peut pas mettre non plus de valeur par defaut (marche sur phpmyadmin)
+);                                      // Need serveur distant :D
 
 CREATE TABLE helper (
   numero_etudiant INT(8) REFERENCES etudiant,
@@ -50,3 +50,14 @@ CREATE TABLE cours(
   id_grp INT(4) REFERENCES groupe,
   PRIMARY KEY(id_mat, id_grp)
 );
+
+INSERT INTO privileges (nom_priv) VALUES ('user');
+INSERT INTO privileges (nom_priv) VALUES ('admin');
+INSERT INTO groupe (filiere,annee) VALUES ('GEI',1);
+INSERT INTO groupe (filiere,annee) VALUES ('Informatique',1);
+INSERT INTO groupe (filiere,annee) VALUES ('MMI',1);
+INSERT INTO groupe (filiere,annee) VALUES ('RT',1);
+INSERT INTO groupe (filiere,annee) VALUES ('GEI',2);
+INSERT INTO groupe (filiere,annee) VALUES ('Informatique',2);
+INSERT INTO groupe (filiere,annee) VALUES ('MMI',2);
+INSERT INTO groupe (filiere,annee) VALUES ('RT',2);

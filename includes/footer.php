1550f147fc06f97a927826dac87db00bdc708a59
Florian Gouarin <florian@gouarin.info>
2015-12-14 22:14:10 +0100

    Ajout includes

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian
+ * Date: 14/12/2015
+ * Time: 21:50
+ */
+?>
+<!-- debut footer -->
+<div id="footer">
+    (c) 2015 Groupe tutorat - INF2A -  En partenariat avec:
+    <div id="logo_partenaire">
+        <img src="Pictures/pulseheberg.png" />
+    </div>
+
+</div>
+<!-- fin footer -->
---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian
+ * Date: 14/12/2015
+ * Time: 21:49
+ */
